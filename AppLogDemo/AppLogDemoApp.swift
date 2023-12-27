31cf671250001cf36c85ba9fd721e16ec9309f6c
BomberFish <bomberfish@bomberfish.ca>
2023-12-26 19:10:17 -0500

    Initial Commit

---
+++ AppLogDemo/AppLogDemoApp.swift
@@ -0,0 +1,14 @@
+// bomberfish
+// AppLogDemoApp.swift – AppLogDemo
+// created on 2023-12-26
+
+import SwiftUI
+
+@main
+struct AppLogDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppLogDemo/ContentView.swift
@@ -0,0 +1,21 @@
+// bomberfish
+// ContentView.swift – AppLogDemo
+// created on 2023-12-26
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
